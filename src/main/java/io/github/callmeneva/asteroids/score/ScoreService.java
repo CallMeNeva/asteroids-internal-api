7bc057c4d6d1f5db60b96ecd0a56478fb871367c
Maxim Altoukhov <86380488+CallMeNeva@users.noreply.github.com>
2022-02-16 08:09:31 +0300

    Add ScoreService

---
+++ src/main/java/io/github/callmeneva/asteroids/score/ScoreService.java
@@ -0,0 +1,31 @@
+package io.github.callmeneva.asteroids.score;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class ScoreService {
+
+    private final ScoreRepository repository;
+
+    @Autowired
+    public ScoreService(ScoreRepository repository) {
+        this.repository = repository;
+    }
+
+    public List<Score> getTop(long n) {
+        return repository.findTopN(n);
+    }
+
+    public List<Score> getTop(long n, String username) {
+        return repository.findTopNByUsername(n, username);
+    }
+
+    @Transactional
+    public void save(String username, long value) {
+        repository.save(username, value);
+    }
+}
