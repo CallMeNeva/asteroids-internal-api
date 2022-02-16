9f698fdb3af35ab4dd128b7646eee4295c69a086
Maxim Altoukhov <86380488+CallMeNeva@users.noreply.github.com>
2022-02-16 08:02:03 +0300

    Add User (oops)

---
+++ src/main/java/io/github/callmeneva/asteroids/user/User.java
@@ -0,0 +1,26 @@
+package io.github.callmeneva.asteroids.user;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users", schema = "asteroids")
+@Data
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private long id;
+
+    @Column(name = "username", nullable = false, length = 20, unique = true)
+    private String username;
+}
