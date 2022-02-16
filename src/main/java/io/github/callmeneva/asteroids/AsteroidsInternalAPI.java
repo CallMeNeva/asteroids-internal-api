d0197d4722b5bdff48e4290f77ca3195e51d11ba
Maxim Altoukhov <86380488+CallMeNeva@users.noreply.github.com>
2022-02-16 20:00:46 +0300

    CORS: register sarchuk.ru

--- src/main/java/io/github/callmeneva/asteroids/AsteroidsInternalAPI.java
+++ src/main/java/io/github/callmeneva/asteroids/AsteroidsInternalAPI.java
@@ -2,11 +2,26 @@ package io.github.callmeneva.asteroids;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class AsteroidsInternalAPI {
 
     public static void main(String[] args) {
         SpringApplication.run(AsteroidsInternalAPI.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer crossOriginConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                CorsRegistration registration = registry.addMapping("/scores");
+                registration.allowedOrigins("https://sarchuk.ru");
+            }
+        };
     }
 }
