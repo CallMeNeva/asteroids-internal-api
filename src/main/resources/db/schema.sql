904b84ddafb2081812c77e8b092f0c739766b71e
Maxim Altoukhov <altoukhov.max@gmail.com>
2022-07-16 13:54:02 +0300

    Change scores.value type from 8-byte to 4-byte uint

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -2,7 +2,7 @@ CREATE SCHEMA IF NOT EXISTS asteroids;
 
 CREATE TABLE IF NOT EXISTS asteroids.players (
     id         SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
-    username   NVARCHAR(20)      NOT NULL,
+    username   NVARCHAR(20)      NOT NULL, -- Username validation is probably better off being done on the application side
     created_on TIMESTAMP         NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT pk_players     PRIMARY KEY (id),
@@ -12,7 +12,7 @@ CREATE TABLE IF NOT EXISTS asteroids.players (
 CREATE TABLE IF NOT EXISTS asteroids.scores (
     id         INT      UNSIGNED NOT NULL AUTO_INCREMENT,
     player_id  SMALLINT UNSIGNED NOT NULL,
-    value      BIGINT   UNSIGNED NOT NULL,
+    value      INT      UNSIGNED NOT NULL,
     created_on TIMESTAMP         NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
     CONSTRAINT pk_scores         PRIMARY KEY (id),
