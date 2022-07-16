bc3d2ec4294864068d6a15dcf5c158cbc56050b7
Maxim Altoukhov <altoukhov.max@gmail.com>
2022-07-16 12:42:15 +0300

    Update schema, split into separate script

---
+++ src/main/resources/db/schema.sql
@@ -0,0 +1,20 @@
+CREATE SCHEMA IF NOT EXISTS asteroids;
+
+CREATE TABLE IF NOT EXISTS asteroids.players (
+    id         SMALLINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    username   NVARCHAR(20)      NOT NULL,
+    created_on TIMESTAMP         NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT pk_players     PRIMARY KEY (id),
+    CONSTRAINT unique_players UNIQUE (username)
+);
+
+CREATE TABLE IF NOT EXISTS asteroids.scores (
+    id         INT      UNSIGNED NOT NULL AUTO_INCREMENT,
+    player_id  SMALLINT UNSIGNED NOT NULL,
+    value      BIGINT   UNSIGNED NOT NULL,
+    created_on TIMESTAMP         NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT pk_scores         PRIMARY KEY (id),
+    CONSTRAINT fk_scores_players FOREIGN KEY (player_id) REFERENCES players (id)
+);
