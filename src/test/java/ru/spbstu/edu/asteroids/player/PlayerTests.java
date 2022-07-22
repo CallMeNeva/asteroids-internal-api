355161f1d1c131b126d875661438bf7e6ebdc8dc
Maxim Altoukhov <altoukhov.max@gmail.com>
2022-07-22 21:31:12 +0300

    Update Player tests

--- src/test/java/ru/spbstu/edu/asteroids/player/PlayerTests.java
+++ src/test/java/ru/spbstu/edu/asteroids/player/PlayerTests.java
@@ -1,5 +1,6 @@
 package ru.spbstu.edu.asteroids.player;
 
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullAndEmptySource;
 import org.junit.jupiter.params.provider.ValueSource;
@@ -8,16 +9,43 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 class PlayerTests {
 
-    @ParameterizedTest
-    @ValueSource(strings = {"AzureDiamond", "exactly20characters!", "with some spaces", "ДругойЯзык"})
-    void acceptsValidUsernames(String username) {
-        assertThat(Player.isUsernameValid(username)).isTrue();
-    }
+    @Nested
+    class UsernameConstraints {
 
-    @ParameterizedTest
-    @ValueSource(strings = {" ", "   ", "\t", "\t\n", "definitelyMoreThan20Characters"})
-    @NullAndEmptySource
-    void rejectsInvalidUsernames(String username) {
-        assertThat(Player.isUsernameValid(username)).isFalse();
+        // Things to ensure:
+        // 1. Not null
+        // 2. Not empty
+        // 3. Length <= 20
+        // 4. Alphanumeric chars only (no whitespace, non-ASCII allowed)
+
+        @ParameterizedTest
+        @NullAndEmptySource
+        @ValueSource(strings = {
+                "ThisHasMoreThan20Characters",
+                "this has spaces",
+                "this\thas\ttabs",
+                "separate\nlines",
+                "with_underscores",
+                "with-hyphens",
+                "with+pluses",
+                "\"Escaped\"Chars",
+                "   "
+        })
+        void rejectsInvalid(String username) {
+            assertThat(Player.isUsernameValid(username)).isFalse();
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {
+                "lowercase",
+                "UPPERCASE",
+                "TwentyCharsMixedCase",
+                "WithNumbers01",
+                "1234567890",
+                "ЗаПределамиASCII"
+        })
+        void acceptsValid(String username) {
+            assertThat(Player.isUsernameValid(username)).isTrue();
+        }
     }
 }
