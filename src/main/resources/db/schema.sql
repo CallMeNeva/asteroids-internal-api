e2ab01960d7bd763619c1285b13a8905183a0593
Maxim Altoukhov <altoukhov.max@gmail.com>
2022-07-23 22:17:41 +0300

    Update schema

--- src/main/resources/db/schema.sql
+++ src/main/resources/db/schema.sql
@@ -1,17 +1,16 @@
 CREATE SCHEMA IF NOT EXISTS asteroids;
 
 CREATE TABLE IF NOT EXISTS asteroids.players (
-    username VARCHAR(20) CHARACTER SET 'ascii' NOT NULL, -- Username validation is done on the application side
-    registered_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    username NVARCHAR(20) NOT NULL, -- Username validation is done on the application side
 
     CONSTRAINT pk_players PRIMARY KEY (username)
 );
 
 CREATE TABLE IF NOT EXISTS asteroids.scores (
-    value INT UNSIGNED NOT NULL,
-    player_username VARCHAR(20) CHARACTER SET 'ascii' NOT NULL,
-    published_on TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    value INT UNSIGNED NOT NULL DEFAULT 0,
+    player_username NVARCHAR(20) NOT NULL,
+    achieved_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
 
-    CONSTRAINT pk_scores PRIMARY KEY (value, player_username, published_on),
+    CONSTRAINT pk_scores PRIMARY KEY (value, player_username, achieved_at),
     CONSTRAINT fk_scores_players FOREIGN KEY (player_username) REFERENCES players (username)
 );
