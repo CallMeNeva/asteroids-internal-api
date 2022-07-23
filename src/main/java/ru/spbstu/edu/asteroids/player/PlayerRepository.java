bd0dc1507de6cbdd9981d66cf892ec3498fcb58d
Maxim Altoukhov <altoukhov.max@gmail.com>
2022-07-23 22:20:51 +0300

    Update PlayerRepository

--- src/main/java/ru/spbstu/edu/asteroids/player/PlayerRepository.java
+++ src/main/java/ru/spbstu/edu/asteroids/player/PlayerRepository.java
@@ -1,19 +1,8 @@
 package ru.spbstu.edu.asteroids.player;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
-public interface PlayerRepository extends JpaRepository<Player, Integer> {
-
-    @Query(value = "SELECT * FROM asteroids.players ORDER BY created_on ASC LIMIT 1", nativeQuery = true)
-    Optional<Player> findOldest();
-
-    @Query(value = "SELECT * FROM asteroids.players ORDER BY created_on DESC LIMIT 1", nativeQuery = true)
-    Optional<Player> findNewest();
-
-    Optional<Player> findByUsername(String username);
+public interface PlayerRepository extends JpaRepository<Player, String> {
 }
