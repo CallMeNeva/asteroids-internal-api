70510b85f747e43bcce52b860d036ec801f02a67
Maxim Altoukhov <altoukhov.max@gmail.com>
2022-07-26 23:28:26 +0300

    Use username ArgumentProvider's in Player tests + treat null as separate case

--- src/test/java/ru/spbstu/edu/asteroids/player/PlayerTests.java
+++ src/test/java/ru/spbstu/edu/asteroids/player/PlayerTests.java
@@ -1,34 +1,34 @@
 package ru.spbstu.edu.asteroids.player;
 
 import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.NullAndEmptySource;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.ArgumentsSource;
 
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 import static org.assertj.core.api.Assertions.assertThatNoException;
+import static org.assertj.core.api.Assertions.assertThatNullPointerException;
 
 class PlayerTests {
 
     @Nested
     class UsernameConstraints {
 
-        // Things to ensure:
-        // 1. Not null
-        // 2. Not blank
-        // 3. Length <= 20
-
         @ParameterizedTest
-        @NullAndEmptySource
-        @ValueSource(strings = {"   ", "\t", "\t \n", "ThisHasMoreThan20Characters"})
-        void rejectsIllegal(String username) {
-            assertThatExceptionOfType(IllegalPlayerUsernameException.class).isThrownBy(() -> new Player(username));
+        @ArgumentsSource(LegalUsernameArgumentsProvider.class)
+        void acceptsLegal(String legalUsername) {
+            assertThatNoException().isThrownBy(() -> new Player(legalUsername));
         }
 
         @ParameterizedTest
-        @ValueSource(strings = {"lower", "UPPER", "TwentyCharsMixedCase", "1234567890", "ЗаПределамиASCII", "with whitespace"})
-        void acceptsLegal(String username) {
-            assertThatNoException().isThrownBy(() -> new Player(username));
+        @ArgumentsSource(IllegalUsernameArgumentsProvider.class)
+        void rejectsIllegal(String illegalUsername) {
+            assertThatExceptionOfType(IllegalPlayerUsernameException.class).isThrownBy(() -> new Player(illegalUsername));
+        }
+
+        @Test
+        void rejectsNull() {
+            assertThatNullPointerException().isThrownBy(() -> new Player(null));
         }
     }
 
