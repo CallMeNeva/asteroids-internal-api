3f032f1e63eca51c9e4a24298a94187bce9b6bb7
Maxim Altoukhov <altoukhov.max@gmail.com>
2022-07-27 15:52:19 +0300

    Update Score

--- src/main/java/ru/spbstu/edu/asteroids/score/Score.java
+++ src/main/java/ru/spbstu/edu/asteroids/score/Score.java
@@ -1,37 +1,122 @@
 package ru.spbstu.edu.asteroids.score;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import ru.spbstu.edu.asteroids.model.BaseEntity;
 import ru.spbstu.edu.asteroids.player.Player;
 
+import javax.persistence.Access;
+import javax.persistence.AccessType;
 import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import java.io.Serializable;
+import java.time.OffsetDateTime;
+import java.util.Objects;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 @Entity
 @Table(name = "scores")
-public class Score extends BaseEntity<Long> {
+public class Score {
 
-    @ManyToOne
-    @JoinColumn(name = "player_id", nullable = false)
-    private Player player;
+    @Embeddable
+    @Access(AccessType.FIELD) // Explicitness just in case, since we don't need property accessors for PK
+    public static class PK implements Serializable {
+
+        @ManyToOne
+        @JoinColumn(name = "player_username", nullable = false)
+        private Player player;
+
+        @Column(name = "achieved_at", nullable = false)
+        private OffsetDateTime achievementDateTime;
+
+        public PK(Player player, OffsetDateTime achievementDateTime) {
+            this.player = Objects.requireNonNull(player);
+            this.achievementDateTime = Objects.requireNonNull(achievementDateTime);
+        }
+
+        public PK() {
+            // JPA spec requirement
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj)
+                return true;
+
+            if (obj == null || getClass() != obj.getClass())
+                return false;
+
+            PK pk = (PK) obj;
+            return Objects.equals(player, pk.player) && Objects.equals(achievementDateTime, pk.achievementDateTime);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(player, achievementDateTime);
+        }
+
+        @Override
+        public String toString() {
+            return String.format("Score.PK[player=%s, achieved=%s]", player.getUsername(), achievementDateTime.toString());
+        }
+    }
 
     @Column(name = "value", nullable = false)
     private long value;
 
-    // TODO: equals and hashCode
+    @EmbeddedId
+    private PK pk;
+
+    public Score(long value, Player player, OffsetDateTime achievementDateTime) {
+        this.value = value;
+        this.pk = new PK(player, achievementDateTime);
+    }
+
+    public Score(long value, Player player) {
+        this(value, player, OffsetDateTime.now());
+    }
+
+    protected Score() {
+        // JPA spec requirement
+    }
+
+    public long getValue() {
+        return value;
+    }
+
+    public void setValue(long value) {
+        this.value = value;
+    }
+
+    public Player getPlayer() {
+        return pk.player;
+    }
+
+    public OffsetDateTime getAchievementDateTime() {
+        return pk.achievementDateTime;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+
+        if (obj == null || getClass() != obj.getClass())
+            return false;
+
+        Score score = (Score) obj;
+        return value == score.value && Objects.equals(pk, score.pk);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value, pk);
+    }
 
     @Override
     public String toString() {
-        return Long.toString(value);
+        Player player = getPlayer();
+        return value + " by <" + player.getUsername() + "> at " + getAchievementDateTime();
     }
 }
